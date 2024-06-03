8e075e9aebd334df0d767e87c7ed8b497b2343fe
dev_abir <59115184+mdabirhasan551@users.noreply.github.com>
2024-06-03 13:46:04 +0600

    chef data added

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const cors = require('cors')
-const data = require('./data/food.json')
-app.get('/', (req, res) => {
-  res.send(data)
-})
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+const foodData = require("./data/food.json");
+const chefData = require("./data/chefs.json");
+
+app.use(cors());
+
+app.get("/food", (req, res) => {
+  res.send(foodData);
+});
+
+app.get("/chef", (req, res) => {
+  res.send(chefData);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`server is running on: ${port}`);
+});
