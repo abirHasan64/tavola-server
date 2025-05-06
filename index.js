6a5d8fba3f8723ff98395cc9b0f9c997afe4ae72
Md. Abir Hasan <155450128+abirHasan64@users.noreply.github.com>
2025-05-07 00:30:02 +0600

    vercel configuration added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get("/food/:category/:id", (req, res) => {
   const category = req.params.category;
   const categoryItems = foodData.filter((item) => item.category === category);
   const selectedFood = categoryItems.find((c) => c.id == id);
-  
+
   if (selectedFood) {
     res.send(selectedFood);
   } else {
@@ -32,6 +32,12 @@ app.get("/food/:category/:id", (req, res) => {
 
 app.get("/chef", (req, res) => {
   res.send(chefData);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefData.find((chef) => chef.id == id);
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
